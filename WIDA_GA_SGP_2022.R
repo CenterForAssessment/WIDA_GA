3b61c8ccef62ee0fdbd464a19a87b6bcec8b4dee
Adam VanIwaarden <avaniwaarden@gmail.com>
2022-10-14 21:34:29 -0600

    Move documentation to bottom of script and test rendering

--- WIDA_GA_SGP_2022.R
+++ WIDA_GA_SGP_2022.R
@@ -1,9 +1,54 @@
-#+ include = FALSE, purl = FALSE
+#+ include = FALSE, purl = FALSE, eval = FALSE
 ###############################################################################
 ###                                                                         ###
 ###          Script to calculate SGPs for 2022 Georgia WIDA-ACCESS          ###
 ###                                                                         ###
 ###############################################################################
+
+###   Load required packages
+require(SGP)
+require(data.table)
+
+###   Load SGP object from 2021 analyses and 2022 cleaned/prepped long data
+load("Data/WIDA_GA_SGP.Rdata")
+load("Data/WIDA_GA_Data_LONG_2022.Rdata")
+
+###   Add baseline matrices to `SGPstateData`
+SGPstateData <-
+    SGPmatrices::addBaselineMatrices(
+        state = "WIDA",
+        year = "2021",
+        add.matrices.to.which.state = "WIDA_GA"
+    )
+
+
+###   Run updateSGP to produce cohort and baseline referrenced SGPs, etc.
+WIDA_GA_SGP <-
+    updateSGP(
+        what_sgp_object = WIDA_GA_SGP,
+        with_sgp_data_LONG = WIDA_GA_Data_LONG_2022,
+        steps = c(
+            "prepareSGP", "analyzeSGP", "combineSGP",
+            "visualizeSGP", "outputSGP"
+        ),
+        sgp.target.scale.scores = TRUE,
+        plot.types = "growthAchievementPlot",
+        outputSGP.output.type = c("LONG_Data", "LONG_FINAL_YEAR_Data"),
+        save.intermediate.results = FALSE,
+        parallel.config = list(
+            BACKEND = "PARALLEL",
+            WORKERS = list(
+                PERCENTILES = 12,
+                PROJECTIONS = 6,
+                LAGGED_PROJECTIONS = 4,
+                SGP_SCALE_SCORE_TARGETS = 4
+            )
+        )
+    )
+
+###   Save results
+save(WIDA_GA_SGP, file = "Data/WIDA_GA_SGP.Rdata")
+
 
 #' ### Conduct SGP analyses
 #' 
@@ -53,49 +98,3 @@
 #' 
 #' District level student data sets with fields used to create individual
 #' student reports were also submitted to GaDOE in Microsoft Excel format.
-
-#+ include = FALSE, purl = FALSE, eval = FALSE
-
-###   Load required packages
-require(SGP)
-require(data.table)
-
-###   Load SGP object from 2021 analyses and 2022 cleaned/prepped long data
-load("Data/WIDA_GA_SGP.Rdata")
-load("Data/WIDA_GA_Data_LONG_2022.Rdata")
-
-###   Add baseline matrices to `SGPstateData`
-SGPstateData <-
-    SGPmatrices::addBaselineMatrices(
-        state = "WIDA",
-        year = "2021",
-        add.matrices.to.which.state = "WIDA_GA"
-    )
-
-
-###   Run updateSGP to produce cohort and baseline referrenced SGPs, etc.
-WIDA_GA_SGP <-
-    updateSGP(
-        what_sgp_object = WIDA_GA_SGP,
-        with_sgp_data_LONG = WIDA_GA_Data_LONG_2022,
-        steps = c(
-            "prepareSGP", "analyzeSGP", "combineSGP",
-            "visualizeSGP", "outputSGP"
-        ),
-        sgp.target.scale.scores = TRUE,
-        plot.types = "growthAchievementPlot",
-        outputSGP.output.type = c("LONG_Data", "LONG_FINAL_YEAR_Data"),
-        save.intermediate.results = FALSE,
-        parallel.config = list(
-            BACKEND = "PARALLEL",
-            WORKERS = list(
-                PERCENTILES = 12,
-                PROJECTIONS = 6,
-                LAGGED_PROJECTIONS = 4,
-                SGP_SCALE_SCORE_TARGETS = 4
-            )
-        )
-    )
-
-###   Save results
-save(WIDA_GA_SGP, file = "Data/WIDA_GA_SGP.Rdata")
